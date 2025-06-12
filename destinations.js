b4861e8166be4cbbc76354e9746d1627f2d5aeae
Venkata Lohith <venkatalohith0991@gmail.com>
2025-06-12 16:16:55 +0530

    Add files via upload

---
+++ destinations.js
@@ -0,0 +1,30 @@
+function myFunction(){
+    alert("Info available Later");
+}
+
+
+function mouseOver1(){
+    document.getElementById("dot1").innerHTML = "<br> <br> <h5 style=\"font-weight: bold; font-family:TimesNewRoman; color: white;\">Embrace a timeless elegance</h5>";
+}
+function mouseOut1(){
+    document.getElementById("dot1").innerHTML = " ";
+}
+function mouseOver2(){
+    document.getElementById("dot2").innerHTML = "<br> <br> <h5 style=\"font-weight: bold; font-family:TimesNewRoman; color: white;\">Indulge in enduring distinction</h5>";
+}
+function mouseOut2(){
+    document.getElementById("dot2").innerHTML = " ";
+}
+function mouseOver3(){
+    document.getElementById("dot3").innerHTML = "<br> <br> <h5 style=\"font-weight: bold; font-family:TimesNewRoman; color: white;\">Dive into a legacy of impeccable service</h5>";
+}
+function mouseOut3(){
+    document.getElementById("dot3").innerHTML = " ";
+}
+function mouseOver4(){
+    document.getElementById("dot4").innerHTML = "<br> <br><h5 style=\"font-weight: bold; font-family:TimesNewRoman; color: white;\">Cultivate cherished memories</h5>";
+    
+}
+function mouseOut4(){
+    document.getElementById("dot4").innerHTML = " ";
+}
